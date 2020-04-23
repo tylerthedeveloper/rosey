83b42479e73956b1a860bed09abe34a73678d0cb
Tyler Citrin <citrin.tyler@gmail.com>
2020-04-23 00:19:58 -0400

    add small form basics, need auto focus and more form features and up to update the model wth it, also tab icon sizing

--- rosey-app/AppNavigation.js
+++ rosey-app/AppNavigation.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useContext, useMemo, useReducer } from 'react';
 import { FlatList, StyleSheet, Button, Text, View, TouchableOpacity } from 'react-native';
+import { MultiBar, MultiBarToggle } from 'react-native-multibar';
 
+/* -------------------------------------------------------------------------- */
+/*                                Auth Section                                */
+/* -------------------------------------------------------------------------- */
 import { createStackNavigator } from '@react-navigation/stack';
 import SigninScreen from './src/screens/SigninScreen';
 import SignupScreen from './src/screens/SignupScreen';
@@ -14,7 +18,13 @@ export const authStackScreen = () => {
         </AuthStack.Navigator>
     );
 }
+/* -------------------------------------------------------------------------- */
 
+
+
+/* -------------------------------------------------------------------------- */
+/*                                List Section                                */
+/* -------------------------------------------------------------------------- */
 import RoseListScreen from './src/screens/RoseListScreen';
 import RoseDetailScreen from './src/screens/RoseDetailScreen';
 
@@ -27,21 +37,156 @@ export const listStack = () => {
         </ListStack.Navigator>
     );
 }
+/* -------------------------------------------------------------------------- */
 
+
+/* -------------------------------------------------------------------------- */
+/*                                 Tab Section                                */
+/* -------------------------------------------------------------------------- */
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MapScreen from './src/screens/MapScreen';
-// import { MultiBar, MultiBarToggle } from 'react-native-multibar';
+import { FontAwesome } from '@expo/vector-icons';
 
 const Tabs = createBottomTabNavigator();
 const TabNavigator = () => {
     return (
         <Tabs.Navigator>
-            <Tabs.Screen name="Map" component={MapScreen} />
-            <Tabs.Screen name="RoseListStack" component={listStack} />
+            <Tabs.Screen name="Map" component={MapScreen}
+                options={{
+                    tabBarLabel: 'Map',
+                    tabBarIcon: ({ color }) => (
+                        <FontAwesome
+                            name="map"
+                            size={24}
+                            style={{ marginBottom: -5 }}
+                        />)
+                }}
+            />
+            <Tabs.Screen name="RoseListStack" component={listStack}
+                options={{
+                    tabBarLabel: 'Roses',
+                    tabBarIcon: ({ color }) => (
+                        <FontAwesome
+                            name="list"
+                            size={24}
+                        />)
+                }} />
         </Tabs.Navigator>
     )
 };
 
+// import { MultiBar, MultiBarToggle } from 'react-native-multibar';
+// const TabNavigator = () => {
+//     const Tabs = useMemo(() => createBottomTabNavigator(), []);
+
+//     return (
+//         <Tabs.Navigator
+//             tabBar={MultiBar}
+//             tabBarOptions={{
+//                 showLabel: false,
+//                 activeTintColor: '#F8F8F8',
+//                 inactiveTintColor: '#586589',
+//                 style: {
+//                     backgroundColor: '#171F33'
+//                 },
+//                 tabStyle: {}
+//             }}
+//         >
+//             <Tabs.Screen
+//                 name="Map"
+//                 component={MapScreen}
+//                 options={{
+//                     tabBarIcon: ({ tintColor }) => (
+//                         <Icon
+//                             name="bookmark"
+//                             color={tintColor}
+//                             size={24}
+//                         />
+//                     )
+//                 }}
+//             />
+//             <Tabs.Screen
+//                 name="MultiBar"
+//                 component={() => <View />}
+//                 options={({ navigation, route }) => ({
+//                     tabBarIcon: () => (
+//                         <MultiBarToggle
+//                             navigation={navigation}
+//                             route={route}
+//                             actionSize={30}
+//                             routes={[
+//                                 {
+//                                     routeName: Routes.OtherScreen,
+//                                     color: '#FF8360',
+//                                     icon: (
+//                                         <Icon
+//                                             name="rocket"
+//                                             color="#333333"
+//                                             size={15}
+//                                         />
+//                                     )
+//                                 },
+//                                 {
+//                                     routeName: Routes.OtherScreen,
+//                                     color: '#E8E288',
+//                                     icon: (
+//                                         <Icon
+//                                             name="dashboard"
+//                                             color="#333333"
+//                                             size={15}
+//                                         />
+//                                     )
+//                                 },
+//                                 {
+//                                     routeName: Routes.OtherScreen,
+//                                     color: '#7DCE82',
+//                                     icon: (
+//                                         <Icon
+//                                             name="gears"
+//                                             color="#333333"
+//                                             size={15}
+//                                         />
+//                                     )
+//                                 }
+//                             ]}
+//                             icon={(
+//                                 <Icon
+//                                     name="plus"
+//                                     color="#FFFFFF"
+//                                     size={24}
+//                                 />
+//                             )}
+//                         />
+//                     )
+//                 })}
+//             />
+//             <Tabs.Screen
+//                 name="RoseListStack"
+//                 component={listStack}
+//                 options={{
+//                     tabBarIcon: ({ tintColor }) => (
+//                         <Icon
+//                             name="lock"
+//                             color={tintColor}
+//                             size={24}
+//                         />
+//                     )
+//                 }}
+//             />
+//         </Tabs.Navigator>
+//     );
+// };
+
+
+
+
+/* -------------------------------------------------------------------------- */
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                               Drawer Section                               */
+/* -------------------------------------------------------------------------- */
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import AccountScreen from './src/screens/AccountScreen';
 import AddRoseScreen from './src/screens/AddRoseScreen';
@@ -51,7 +196,11 @@ export const App = () => {
     return (
         <Drawer.Navigator initialRouteName="Main">
             <Drawer.Screen name="Account" component={AccountScreen} />
-            <Drawer.Screen name="AddRose" component={AddRoseScreen} />
+            <Drawer.Screen name="AddRose" component={AddRoseScreen}
+                options={{
+                    title: "Add new Rose"
+                }}
+            />
             <Drawer.Screen name="Main" component={TabNavigator} />
         </Drawer.Navigator >
     );
@@ -78,7 +227,7 @@ export const App = () => {
 //                                     navigation.dispatch(DrawerActions.toggleDrawer());
 //                                 }}>
 //                                 <Text>Open</Text>
-//                                 {/* <Image source={require('./assets/images/icons/drawer.png')} /> */}
+                                // {/* <Image source={require('./assets/images/icons/drawer.png')} /> */}
 //                             </TouchableOpacity>
 //                         </View>
 //                     }
@@ -87,5 +236,6 @@ export const App = () => {
 //         </DrawerStack.Navigator>
 //     );
 // }
+/* -------------------------------------------------------------------------- */
 
 
--- rosey-app/src/components/RoseForm.js
+++ rosey-app/src/components/RoseForm.js
@@ -1,64 +1,85 @@
 import React, { useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ScrollView } from 'react-native';
 import { Button, Input, Text } from 'react-native-elements';
 import Spacer from './Spacer';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { Form } from '@secullum/react-native-autofocus'
 
 const RoseForm = ({ headerText, submitButtonText, onSubmit, onCancel }) => {
 
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
-    // const [placeMetAt, setPlace] = useState({
-    //     timestamp: 0,
-    //     placeName: '',
-    //     coords: {
-    //         latitude: 0,
-    //         longitude: 0,
-    //     },
-    // });
+    const [phoneNumber, setPhoneNumber] = useState('');
     const [placeName, setPlace] = useState('');
     const [coords, setCoords] = useState({ latitude: -369, longitude: -369 })
-    // const [dateMet, setDateMet] = useState(Date.now())
     const [picture, setPicture] = useState('');
-
     const placeMetAt = {
         placeName,
         coords,
     };
 
-    const dateMet = Date.now();
+    /* -------------------------------------------------------------------------- */
+    /*                                Date Section                                */
+    /* -------------------------------------------------------------------------- */
+    const [dateMet, setDate] = useState(Date.now());
+    const [mode, setMode] = useState('date');
+
+    const onChange = (event, selectedDate) => {
+        const currentDate = selectedDate || dateMet;
+        // setShow(Platform.OS === 'ios');
+        setDate(currentDate);
+    };
+    console.log(Date.parse(dateMet));
+    /* -------------------------------------------------------------------------- */
+
     return (
-        <>
+        <ScrollView>
+            <Form focusOn={[TextInput]}>
             <Spacer>
                 <Spacer>
                     <Text h3> {headerText} </Text>
                 </Spacer>
                 <Spacer>
-                    <Input label="Name"
+                    <TextInput label="Name"
                         value={name}
                         onChangeText={setName}
                         autoCorrect={false}
+                        returnKeyType={"next"}
                     />
                 </Spacer>
                 <Spacer>
-                    <Input label="Email"
+                    <TextInput label="Email"
                         value={email}
                         onChangeText={setEmail}
                         autoCorrect={false}
+                        keyboardType={"email-address"}
+                    />
+                </Spacer>
+                <Spacer>
+                    <TextInput label="Phone Number"
+                        value={phoneNumber}
+                        onChangeText={setPhoneNumber}
+                        keyboardType={"phone-pad"}
                     />
                 </Spacer>
                 <Spacer>
                     <Input label="Place Met At"
                         value={placeName}
                         onChangeText={setPlace}
+                        autoCorrect={false}                        
                     />
                 </Spacer>
-                {/* <Spacer>
-                     <Input label="Date Met"
+                <Spacer>
+                    <DateTimePicker
+                        testID="dateTimePicker"
+                        timeZoneOffsetInMinutes={0}
                         value={dateMet}
-                        // textContentType={""}
-                        onChangeText={setDateMet}
+                        mode={mode}
+                        is24Hour={true}
+                        display="default"
+                        onChange={onChange}
                     />
-                </Spacer> */}
+                </Spacer>
                 <Spacer>
                     <Input label="Picture"
                         value={picture}
@@ -69,7 +90,7 @@ const RoseForm = ({ headerText, submitButtonText, onSubmit, onCancel }) => {
                 <Spacer>
                     <Button
                         title={submitButtonText}
-                        onPress={() => onSubmit({ name, email, picture, placeMetAt, dateMet })}
+                        onPress={() => onSubmit({ name, email, picture, placeMetAt, dateMet, phoneNumber })}
                     />
                 </Spacer>
                 <Spacer>
@@ -79,7 +100,8 @@ const RoseForm = ({ headerText, submitButtonText, onSubmit, onCancel }) => {
                     />
                 </Spacer>
             </Spacer>
-        </>);
+            </Form>
+        </ScrollView>);
 }
 
 const styles = StyleSheet.create({
