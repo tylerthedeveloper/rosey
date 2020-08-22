8e77261ac038232a3bceefe365befab663228390
Tyler Citrin <tcitrin@indiana.edu>
2020-08-22 16:40:42 -0700

    set 1.7.1 build for qr code fix

--- app.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  extra: {
-    EXPO_APPLE_ID: process.env.EXPO_APPLE_ID,
-    EXPO_APPLE_PASSWORD: process.env.EXPO_APPLE_PASSWORD,
-    bundleIdentifier: "com.rosey-app.rosey"
-  },
-};
